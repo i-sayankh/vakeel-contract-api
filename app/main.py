23afdbac88061d41e8f17a3edf69bd2d0f420b2d
Sayan Khutia <sayank.khutia123@gmail.com>
2026-09-07 19:14:33 +0530

    project set up

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Vakeel Contracts API",
+    description="AI Powered contract analysis using Gemini API",
+    version="0.0.1",
+)
+
+
+@app.get("/")
+async def root():
+    return {
+        "app": "Vakeel Contract API",
+        "version": "0.0.1",
+        "endpoints": {
+            "POST /contracts/upload": "Upload a PDF or TXT contract for analysis",
+            "GET /contracts/": "Retrieve a list of all uploaded contracts",
+            "GET /contracts/{id}": "Retrieve details of a specific contract by ID",
+            "POST /analysis/analyse/{contract_id}": "Analyze a contract using AI and return insights",
+            "GET /analysis/{analysis_id}": "Retrieve the results of a specific analysis by ID",
+            "GET /analysis/contract/{contract_id}": "Retrieve a list of all analyses performed for a specific contract",
+        },
+    }
